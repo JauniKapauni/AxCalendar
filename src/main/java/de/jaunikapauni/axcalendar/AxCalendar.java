8c618c083e25d4f1a11abba1dbeb840d33bf8745
JauniKapauni <riches-goods.54@icloud.com>
2026-05-20 16:56:55 +0200

    feat: add CalendarCommand

--- src/main/java/de/jaunikapauni/axcalendar/AxCalendar.java
+++ src/main/java/de/jaunikapauni/axcalendar/AxCalendar.java
@@ -1,16 +1,29 @@
 package de.jaunikapauni.axcalendar;
 
-import org.bukkit.plugin.java.JavaPlugin;
+import de.jaunikapauni.axcalendar.command.CalendarCommand;
+import de.jaunikapauni.axcalendar.listener.InventoryClickListener;import org.bukkit.Bukkit;
+import org.bukkit.Material;import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxCalendar extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getCommand("calendar").setExecutor(new CalendarCommand(this));
+        getServer().getPluginManager().registerEvents(new InventoryClickListener(), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public void openCalendarGUI(Player p){
+        Inventory gui = Bukkit.createInventory(null, 9, "Inventory");
+        ItemStack reward = new ItemStack(Material.COAL_BLOCK);
+        gui.setItem(0, reward);
+        p.openInventory(gui);
     }
 }
---
+++ src/main/java/de/jaunikapauni/axcalendar/command/CalendarCommand.java
@@ -0,0 +1,21 @@
+package de.jaunikapauni.axcalendar.command;
+
+import de.jaunikapauni.axcalendar.AxCalendar;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class CalendarCommand implements CommandExecutor {
+    AxCalendar reference;
+    public CalendarCommand(AxCalendar reference){
+        this.reference = reference;
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        Player p = (Player) sender;
+        reference.openCalendarGUI(p);
+        return true;
+    }
+}
