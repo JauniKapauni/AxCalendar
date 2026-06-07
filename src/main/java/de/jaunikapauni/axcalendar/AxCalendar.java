3211327584790c9c0d59dbe190351c20bbe8e95b
JauniKapauni <riches-goods.54@icloud.com>
2026-06-07 22:39:28 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axcalendar/AxCalendar.java
+++ src/main/java/de/jaunikapauni/axcalendar/AxCalendar.java
@@ -2,6 +2,7 @@ package de.jaunikapauni.axcalendar;
 
 import de.jaunikapauni.axcalendar.command.CalendarCommand;
 import de.jaunikapauni.axcalendar.listener.InventoryClickListener;
+import de.jaunikapauni.axcalendar.manager.DatabaseManager;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -11,11 +12,23 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxCalendar extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         saveDefaultConfig();
+        try{
+            databaseManager = new DatabaseManager(this);
+            if(databaseManager.initDatabaseTable1() == false){
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         getCommand("calendar").setExecutor(new CalendarCommand(this));
         getServer().getPluginManager().registerEvents(new InventoryClickListener(this), this);
     }
