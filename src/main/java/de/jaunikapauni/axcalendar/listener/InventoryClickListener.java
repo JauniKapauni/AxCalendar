ee6097b103989291ad16e065917714ea93754fe3
JauniKapauni <riches-goods.54@icloud.com>
2026-06-07 23:12:32 +0200

    fix: add check via date objects

--- src/main/java/de/jaunikapauni/axcalendar/listener/InventoryClickListener.java
+++ src/main/java/de/jaunikapauni/axcalendar/listener/InventoryClickListener.java
@@ -7,6 +7,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.sql.*;
+import java.time.LocalDate;
+
 public class InventoryClickListener implements Listener {
     AxCalendar reference;
     public InventoryClickListener(AxCalendar reference){
@@ -21,8 +24,46 @@ public class InventoryClickListener implements Listener {
                 Player p = (Player) e.getWhoClicked();
                 ItemStack reward = e.getInventory().getItem(0);
                 if(reward != null){
-                    String cmd = reference.getConfig().getString("calendar.day1.command");
-                    p.performCommand(cmd);
+                    Date last = null;
+                    try(Connection conn = reference.getDatabaseManager().getConnection()){
+                        try(PreparedStatement ps = conn.prepareStatement("SELECT * FROM players WHERE uuid = ?")){
+                            ps.setString(1, p.getUniqueId().toString());
+                            ResultSet rs = ps.executeQuery();
+                            if(rs.next()){
+                                last = rs.getDate("last");
+                            }
+                        }
+                    } catch (SQLException ex) {
+                        throw new RuntimeException(ex);
+                    }
+                    if(last == null){
+                        try(Connection conn = reference.getDatabaseManager().getConnection()){
+                            try(PreparedStatement ps = conn.prepareStatement("INSERT INTO players (uuid, last) VALUES (?, NOW())")){
+                                ps.setString(1, p.getUniqueId().toString());
+                                ps.executeUpdate();
+                                String cmd = reference.getConfig().getString("calendar.day1.command");
+                                p.performCommand(cmd);
+                                return;
+                            }
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    Date now = Date.valueOf(LocalDate.now());
+                    if(last != null && last.equals(now)){
+                        p.sendMessage("Sorry wait until the start of the next day!");
+                    } else {
+                        String cmd = reference.getConfig().getString("calendar.day1.command");
+                        p.performCommand(cmd);
+                        try(Connection conn2 = reference.getDatabaseManager().getConnection()){
+                            try(PreparedStatement ps2 = conn2.prepareStatement("UPDATE players SET last = NOW() WHERE uuid = ?")){
+                                ps2.setString(1, p.getUniqueId().toString());
+                                ps2.executeUpdate();
+                            }
+                        } catch (SQLException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
                 }
             }
         }
