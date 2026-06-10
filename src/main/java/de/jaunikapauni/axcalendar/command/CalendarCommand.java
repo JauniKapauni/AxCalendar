56d56cee649e4cc4c20bd95013afeae77a8e44e0
JauniKapauni <riches-goods.54@icloud.com>
2026-06-10 09:43:36 +0200

    fix: add perm check

--- src/main/java/de/jaunikapauni/axcalendar/command/CalendarCommand.java
+++ src/main/java/de/jaunikapauni/axcalendar/command/CalendarCommand.java
@@ -14,7 +14,15 @@ public class CalendarCommand implements CommandExecutor {
     }
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+        if(!(sender instanceof Player)){
+            sender.sendMessage("Only players can run this commmand!");
+            return true;
+        }
         Player p = (Player) sender;
+        if(!p.hasPermission("axcalendar.calendar")){
+            p.sendMessage("You don't have the permission!");
+            return true;
+        }
         reference.openCalendarGUI(p);
         return true;
     }
